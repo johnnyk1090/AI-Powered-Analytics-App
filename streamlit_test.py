1193d1015e2d94292cc118d43ee4ae26af5de796
johnnyk1090 <konteas17giannis@gmail.com>
2024-06-27 01:33:28 +0300

    .

--- streamlit_test.py
+++ streamlit_test.py
@@ -41,28 +41,6 @@ login(token=hf_token, add_to_git_credential=True)
 
 # ------------------------    CODING SECTION  ----------------------------
 
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(jpg_file):
-    bin_str = get_base64_of_bin_file(jpg_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-        background-image: url("data:image/jpeg;base64,%s");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-position: center;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('images/36ff87fa-f66f-4794-a0c7-f9e6ad826917.jpeg')
-
 # Function to reset chat
 def reset_chat():
     st.session_state.messages = []
@@ -222,7 +200,29 @@ with st.sidebar:
     else:
         st.info("Please upload a file.")            
         
-# ------------------------    CSS SECTION  ----------------------------        
+# ------------------------    CSS SECTION  ----------------------------
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(jpg_file):
+    bin_str = get_base64_of_bin_file(jpg_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+        background-image: url("data:image/jpeg;base64,%s");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: center;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('images/36ff87fa-f66f-4794-a0c7-f9e6ad826917.jpeg')        
 
 # CSS to make the header white and bold
 header_style = '''
